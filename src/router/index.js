0e31262bf70b7180033a8b46455c1678e4475ef4
dinieva <88323489+dinieva@users.noreply.github.com>
2023-04-27 18:27:28 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from "@/views/ HomePage.vue"
 import  AboutPage from "@/views/AboutPage.vue"
-
+import NotFound from "@/views/NotFound.vue"
 const routes = [
   { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
+  { path: "/:catchAll(.*)", component: NotFound },   /* :catchAll является динамическим сегментом и (.*)представляет собой регулярное выражение,
+ которое по существу соответствует чему угодно. Поэтому, если ни один из маршрутов выше этого не совпадает, он будет использовать этот.  */
 ]
 
 const router = createRouter({
