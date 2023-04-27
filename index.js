ef5d3872e93e24a2a6fe27be7a5cd5424d4d1acc
dinieva <88323489+dinieva@users.noreply.github.com>
2023-04-27 17:59:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import HomePage from "@/views/ HomePage.vue"
 import  AboutPage from "@/views/AboutPage.vue"
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
 ]
 
